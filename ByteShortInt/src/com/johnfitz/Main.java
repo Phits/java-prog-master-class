258a3cf86cd79bc84c5dbd7780df181a6644f7ae
phits <john@fitz.com>
2019-06-02 21:12:31 -0400

    Started bytes

---
+++ ByteShortInt/src/com/johnfitz/Main.java
@@ -0,0 +1,20 @@
+package com.johnfitz;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // int has a width of 32
+	    int myMinValue = -2_147_483_648;
+	    int myMaxValue = 2_147_483_647;
+
+        // byte has a width of 8
+	    byte myByteValue = -128;
+
+        // short has a width of 16
+	    short myShortValue = 32767;
+
+        // short has a width of 64
+	    long myLongValue = -9_223_372_036_854_775_808L;
+    }
+}
